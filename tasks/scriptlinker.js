d5809d2580c4f843fd926f28dce3349d82d00e35
Ryan Whitmire <ryanw51@gmail.com>
2015-01-25 10:40:15 -0500

    move logic into separate module for easier testing

--- tasks/scriptlinker.js
+++ tasks/scriptlinker.js
@@ -6,9 +6,10 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
 
-var util = require('util');
+//var util = require('util');
+
+var linker = require('../lib/linker');
 
 module.exports = function(grunt) {
 
@@ -70,77 +71,9 @@ module.exports = function(grunt) {
     //   });
     // });
 
-
-    /**
-     * gets a list of script blocks from html content
-     * @param  {string} htmlContent html content
-     * @return {array}  list of script block meta data
-     */
-    function getBlockDefs(htmlContent) {
-      var matches = htmlContent.match(/<!--\s*SCRIPTS\s*src.*-->/g);
-
-      return matches.map(function(blockStart) {
-        var src = /src="(.*)"/g.exec(blockStart)[1];
-        return {blockStart: blockStart, src: src};
-      });
-    }
-
-    /**
-     * given a list of paths, formats a sting of scripts to include in html
-     * @param  {array}  paths list of paths to include
-     * @return {string} stringified script tags
-     */
-    function getScriptTags(paths) {
-      var scripts = '';
-
-      paths.forEach(function(path) {
-        var script = '\n<script src="'+ path +'"></script>';
-        scripts += script;
-      });
-
-      scripts += '\n';
-
-      return scripts;
-    }
-
-
-    /**
-     * processes content of a single html file
-     * @param  {object} htmlFile file information from grunt
-     */
-    function handleHtmlFile(htmlFile) {
-      var htmlFileSrc = htmlFile.src[0];
-      var htmlContent = grunt.file.read(htmlFileSrc);
-
-      // now that we have the html content, we need to use regular expressions to
-      // find all script blocks that will append scripts to the html file
-      var blockDefs = getBlockDefs(htmlContent);
-
-      // loop through blockDefs and inject scripts into html
-      blockDefs.forEach(function(blockDef) {
-
-        // todo: remove hard coded test/ string
-        var scriptPaths = grunt.file.expand( 'test/' + blockDef.src);
-
-        // todo: make sure we have all glob special characters escaped
-        var blockStartExpression = blockDef.blockStart
-          .replace(/\//g, '\\/')
-          .replace(/\*/g, '\\*');
-
-        var start = "(" + blockStartExpression + ")";
-        var end = /(<!--\s*SCRIPTS END\s*-->)/g;
-
-        var expression = new RegExp(start + /\s*/g.source + end.source);
-        var scriptTags = getScriptTags(scriptPaths);
-        htmlContent = htmlContent.replace(expression, "$1" + scriptTags + "$2");
-
-      });
-
-      grunt.file.write(htmlFileSrc, htmlContent);
-
-    }
-
-    this.files.forEach(handleHtmlFile);
+    this.files.forEach(function(file){
+      linker.injectScripts(file, grunt);
+    });
 
   });
 
